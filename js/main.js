55ef5d87445535262e228bff6d8e62f1532af9ed
Arslan <rauros@list.ru>
2019-05-28 13:42:52 +0300

    summ

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 var app = new Vue({
   el: '.app',
   data: {
+    first: 0,
+    second: 0,
     modalOpened: false,
     result: 'result here'
   },
   methods: {
     sum(a, b) {
-      return a + b;
+      if (isNaN(a) || isNaN(b)) {
+        return 'Введите число';
+      } else {
+        return parseFloat(a) + parseFloat(b);
+      }
     },
     showResult () {
-      this.result = this.sum(2, 3)
+      this.result = this.sum(this.first, this.second)
       this.modalOpened = true;
     },
     closeResult () {
